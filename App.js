1324bfc556fcd768ea55426308ef30a2e0e6a2f2
asbonato <asbonato@gmail.com>
2021-05-21 22:37:31 -0300

    Inicio da apostila 5 de react native

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, Keyboard, StyleSheet, TextInput, View, Text } from 'react-native';
 
 export default function App() {
+  const endPoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt&units=metric&q="
+  const apiKey = "sua chave de api";
+  
+  const [cidade, setCidade] = useState('');
+  const [previsoes, setPrevisoes] = useState([]);
+  
+  const capturarCidade = (cidade) => {
+    setCidade(cidade);
+  }
+
+  const obtemPrevisoes = () => {
+    setPrevisoes([]);
+    const target = endPoint + cidade + "&appid=" + apiKey;
+
+    fetch(target)
+    .then((dados) => dados.json())
+    .then((dados) => {
+      setPrevisoes(dados["list"])
+      Keyboard.dismiss()
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput
+          style={styles.nomeCidade}
+          placeholder="Digite o nome de uma cidade"
+          value={cidade}
+          onChangeText={capturarCidade}
+        />
+        <Button
+          title="Ok"
+          onPress={obtemPrevisoes}
+        />
+      </View>
+        <FlatList
+          data={previsoes}
+          renderItem={
+            previsao => (
+              <Text>{JSON.stringify(previsao)}</Text>
+            )
+          }
+        />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 40,
+    flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  nomeCidade: {
+    padding: 10,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    flexGrow: 0.9
+  },
+  entrada: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 8
+  }
+
 });
